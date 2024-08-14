2b7efd9c0a3c6ebf21b2f7324d0667aa1dbcaddf
shanushihab <shanushihab94@gmail.com>
2024-08-14 12:32:57 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.my-masonry-grid').masonry({
+        itemSelector: '.grid-item',
+        columnWidth: '.grid-item',
+        percentPosition: true
+    });
+});
